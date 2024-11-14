b517121d59428c92ecaade1fc3200b46774fd2b2
Tim Walkom <timwalkom@hotmail.com>
2024-11-14 16:38:47 +1030

    Completed the website

--- myScript.js
+++ myScript.js
@@ -3,6 +3,17 @@ let dropDownMenu = document.querySelector('.drop-down-menu');
 
 hamburger.addEventListener('click', () => {
     dropDownMenu.classList.toggle('show');
+    dropDownMenu.classList.toggle('hide');
     hamburger.classList.toggle('flip');
 })
+
+
+let button = document.querySelectorAll('.button-to-booking');
+
+button.forEach(button => {
+    button.addEventListener('click', (event) => {
+        window.location.href = 'contact.html';
+    })
+})
+
 
